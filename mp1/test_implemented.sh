cad7e4b507a35b832e533f47f7a0a8d34d2b65db
Sanjit Kumar <sanjitk2018@gmail.com>
2023-03-05 20:48:58 -0600

    ADD: shell script to test implemented sections

---
+++ mp1/test_implemented.sh
@@ -0,0 +1,12 @@
+echo "\n-----------Running mp1indexing-----------\n"
+make run file="data/text/mp1indexing.txt"
+echo "\n-----------Running mp1req1-----------\n"
+make run file="data/text/mp1req1.txt"
+echo "\n-----------Running mp1req2-----------\n"
+make run file="data/text/mp1req2.txt"
+echo "\n-----------Running mp1depth-----------\n"
+make run file="data/text/mp1depth.txt"
+echo "\n-----------Running mp1srgb-----------\n"
+make run file="data/text/mp1srgb.txt"
+
+echo "\n-----------scipt finished!-----------"
